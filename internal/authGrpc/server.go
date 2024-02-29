76ddde5d871282eead7441b3ba5198b1f3295fe4
perminov.aleksey5 <perminov.aleksey5@wb.ru>
2024-02-29 22:25:24 +0500

    fixed

--- internal/authGrpc/server.go
+++ internal/authGrpc/server.go
@@ -25,11 +25,11 @@ func Register(gRPC *grpc.Server) {
 	ssov1.RegisterAuthServer(gRPC, &GrpcServerApiV1{})
 }
 
-func (ssov1 *GrpcServerApiV1) Register(ctx context.Context, req *ssov1.RegisterRequest) (resp *ssov1.RegisterResponse, err error) {
+func (srv *GrpcServerApiV1) Register(ctx context.Context, req *ssov1.RegisterRequest) (resp *ssov1.RegisterResponse, err error) {
 	return resp, nil
 }
 
-func (ssov1 *GrpcServerApiV1) Logining(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
+func (srv *GrpcServerApiV1) Logining(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
 
 	if req.GetEmail() == "" {
 		return nil, status.Error(codes.InvalidArgument, "email is required")
@@ -62,25 +62,26 @@ func (ssov1 *GrpcServerApiV1) Logining(ctx context.Context, req *ssov1.LoginRequ
 		return nil, status.Error(codes.InvalidArgument, "app id is required")
 	}
 
-	return &ssov1.LoginResponse{
+	resp := &ssov1.LoginResponse{
 		Token: "",
-	}, nil
+	}
+	return resp, nil
 }
 
-func (ssov1 *GrpcServerApiV1) Run(cfg models.GrpcConfig) error {
+func (srv *GrpcServerApiV1) Run(cfg models.GrpcConfig) error {
 	const op = "authGrpc.Run"
 
 	listner, err := net.Listen("tcp", ":"+cfg.Port)
 	if err != nil {
 		return fmt.Errorf("%s %v", op, err)
 	}
-	ssov1.lisner = listner
+	srv.lisner = listner
 
 	return nil
 }
 
-func (ssov1 *GrpcServerApiV1) Shutdown(ctx context.Context) error {
-	return ssov1.lisner.Close()
+func (srv *GrpcServerApiV1) Shutdown(ctx context.Context) error {
+	return srv.lisner.Close()
 }
 
 func verifyPassword(s string) (sevenOrMore, number, upper, special bool) {
